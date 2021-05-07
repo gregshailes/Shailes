91b709791a67ad562ab29641791a2fe615e41cf3
Greg Shailes <greg@coherent.work>
2021-05-07 09:51:54 +0100

    attempt to make devise stylesheets precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.1'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -15,4 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Rails.application.config.assets.precompile += %w( .css )
 
-Rails.application.config.assets.precompile += ['app/assets/sessions.scss']
+Rails.application.config.assets.precompile += %w( devise/registrations.css devise/sessions.css )
