a5deb21638d2a8046e62136198fdd6b71cfbf018
Greg Shailes <greg@coherent.work>
2021-05-17 12:53:08 +0100

    change upcoming bills to 6 months instead of 2

--- app/helpers/finances/accounts_helper.rb
+++ app/helpers/finances/accounts_helper.rb
@@ -5,7 +5,7 @@ module Finances::AccountsHelper
     if account.bills.any?
       bills = account.bills.order(:next_due_at).to_a
       start_from = bills.first.next_due_at
-      ((start_from)..(Date.current + 2.months)).each do |date|
+      ((start_from)..(Date.current + 6.months)).each do |date|
         bills.select{|a| a[:next_due_at] == date}.each do |bill|
           upcoming_bills << {bill_id: bill.id, date: date, transaction: bill.create_payment_transaction}
         end
