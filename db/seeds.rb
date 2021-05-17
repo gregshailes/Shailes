db65a099cd4fbde87c3a6319b72bbbace486cc8b
Greg Shailes <greg@coherent.work>
2021-05-17 15:43:39 +0100

    slightly more refined Greghome menu styling.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  first_name: 'Greg',
+  last_name: 'Shailes',
+  email: 'greg.shailes@gmail.com',
+  password: 'changeme',
+  password_confirmation: 'changeme'
+)
