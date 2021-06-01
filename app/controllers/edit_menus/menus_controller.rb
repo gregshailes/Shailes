3216a289b894dcc3fdf89b314c246d11a62fced6
Greg Shailes <greg@coherent.work>
2021-06-01 17:15:17 +0100

    Add ability to create new menus

--- app/controllers/edit_menus/menus_controller.rb
+++ app/controllers/edit_menus/menus_controller.rb
@@ -1,6 +1,7 @@
 class EditMenus::MenusController < EditMenusController
 
   before_action :set_menu, only: [:edit, :update, :destroy]
+  before_action :set_area, only: [:new, :create]
 
   def update
     @menu.update_attribute(:title, menu_params[:title])
@@ -14,7 +15,15 @@ class EditMenus::MenusController < EditMenusController
   end
 
   def new
+    @menu = Menu.new(menu_area: @area)
+  end
 
+  def create
+    @menu = Menu.new(menu_area: @area)
+    @menu.title = menu_params[:title]
+    if @menu.save!
+      redirect_to edit_menus_area_path(@area) 
+    end
   end
 
   private
