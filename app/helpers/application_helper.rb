2ebfb5462bfad04e4c46194f70e5f78c38c3fdd4
Greg Shailes <greg@coherent.work>
2021-11-02 11:27:15 +0000

    update markdown to allow code blocks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,14 @@ module ApplicationHelper
   private
 
   def renderer
-    @renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::XHTML.new(render_options = {}), extensions = {no_intra_emphasis: true})
+    @renderer ||= Redcarpet::Markdown.new(
+      Redcarpet::Render::XHTML.new(render_options = {}),
+      extensions = {
+        no_intra_emphasis: true,
+        fenced_code_blocks: true,
+        disable_indented_code_blocks: true
+      }
+    )
   end
 
 end
