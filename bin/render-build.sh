a7956540c474ce7b55eeb3d2a4412775b921d2f0
Greg Shailes <greg@coherent.work>
2023-07-17 16:14:58 +0100

    attempt

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,10 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# bundle install
+
 bundle exec rake db:seed
 bundle exec rake db:migrate
+
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
