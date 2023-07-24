3ee16ae2ae81f6441fe0159c70ad7b3e325a2b21
Greg Shailes <greg@coherent.work>
2023-07-24 11:00:08 +0100

    add migration to seed menus

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_120347) do
+ActiveRecord::Schema.define(version: 2023_07_24_110500) do
 
   create_table "boris_cam_images", force: :cascade do |t|
     t.string "filename"
